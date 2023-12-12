7689b55f27cf3df0877df7a09ea5249e510ab777
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-12-12 12:29:42 -0500

    inicio

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,11 @@
+public class GuessTheNumber {
+    public static void main (String[] args) {
+        System.out.println(abcd());
+    }
+
+    public static String abcd() {
+        String nombre = "hola mundooooooo";
+        return nombre;
+    }
+
+}
